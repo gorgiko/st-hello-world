cdf56eb74a3e41d74657825ad6d8e12342e038ba
gorgiko <122184841+gorgiko@users.noreply.github.com>
2025-04-04 00:13:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.set_page_config(layout="wide", page_title="Image Background Remover")
 
 st.write("## Test sajt")
 st.write(
-    ":dog: Try uploading an image to watch the background magically removed. Full quality images can be downloaded from the sidebar. This code is open source and available [here](https://github.com/tyler-simons/BackgroundRemoval) on GitHub. Special thanks to the [rembg library](https://github.com/danielgatis/rembg) :grin:"
+    ":dog: This code is open source and available [here](https://github.com/tyler-simons/BackgroundRemoval) :grin:"
 )
 st.sidebar.write("## Upload and download :gear:")
 
