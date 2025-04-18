9d2f28094ec6b6cb3b7a33686ef11273a9cc2e4c
gorgiko <122184841+gorgiko@users.noreply.github.com>
2025-04-18 19:27:14 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ import time
 
 st.set_page_config(layout="wide", page_title="Image Background Remover")
 
-st.write("## Test sajt")
+st.write("## Тест сајт")
 st.write(
     ":dog: This code is open source and available [here](https://github.com/tyler-simons/BackgroundRemoval) :grin:"
 )
